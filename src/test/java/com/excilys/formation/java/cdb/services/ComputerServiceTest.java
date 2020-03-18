747a1c41954746b06d8f6d7a19dd2e4f47a3db4a
laetitia <laetitia.tureau@orange.fr>
2020-03-18 17:02:00 +0100

    début tests

---
+++ src/test/java/com/excilys/formation/java/cdb/services/ComputerServiceTest.java
@@ -0,0 +1,13 @@
+package com.excilys.formation.java.cdb.services;
+
+import org.junit.Test;
+
+public class ComputerServiceTest {
+    @Test
+    public void updateIntroduced() {
+    }
+
+    @Test
+    public void updateDiscontinued() {
+    }
+}
